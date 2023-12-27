3021e5f1aa6f95ae253a460d378195e954f561aa
Salsabila Istasya <sabilbinchan@gmail.com>
2023-12-27 09:01:34 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,100 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/feather-icons/dist/feather.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+
+    <div class="container-scroller">
+        <div class="row justify-content-center">
+            <div class="content-wrapper d-flex align-items-center auth">
+                <div class="row flex-grow">
+                    <div class="col-lg-4 mx-auto">
+                        {{-- <div class="auth-form-light text-left p-5"> --}}
+                            <div class="brand-logo text-center"></div>
+                        {{-- <img class="img-fluid" src="{{ asset('img/login.png') }}" alt="Logo"> --}}
+                        </div>
+                    <div class="auth-form-light text-left p-5">
+                        <div class="mdi mdi-chef-hat text-purple">
+                            <h1 class="text-center">BERAJAH</h1>
+
+                            <form class="pt-3" action="{{ route('login') }}" method="post">
+                                @csrf
+                                <div class="form-group">
+                                    <div class="input-group mb-3">
+                                    <label>Email <span class="login-danger">*</span></label>
+                                    <input class="form-control" type="email" name="email" value="{{ old('email') }}"
+                                        placeholder="Email" required autocomplete="email" autofocus>
+                                        <div class="input-group-append input-group-text">
+                                    <span class="profile-views"><i class="fas fa-envelope"></i></span>
+                                    </div>
+                                </div>
+                                @error('email')
+                                <div class="form-group">
+                                    <span class="text-danger">{{ $message }}</span>
+                                </div>
+                                @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="input-group mb-3">
+                                    <label>Password <span class="login-danger">*</span></label>
+                                    <input class="form-control pass-input" type="password" name="password"
+                                        placeholder="Password" required autocomplete="current-password">
+                                        <div class="input-group-append input-group-text">
+                                    <span class="profile-views feather-eye toggle-password"></span>
+                                        </div>
+                                </div>
+                                @error('password')
+                                <div class="form-group">
+                                    <span class="text-danger">{{ $message }}</span>
+                                </div>
+                                @enderror
+                                </div>
+
+                                <div class="forgotpass">
+                                    <div class="remember-me">
+                                        <label class="custom_check mr-2 mb-0 d-inline-flex remember-me"> Remember me
+                                            <input type="checkbox" name="remember">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button class="btn btn-primary btn-block" type="submit">Login</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+</body>
+
+        {{-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
@@ -53,8 +144,8 @@
 
                         <div class="row mb-0">
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4"> 
-                                <button type="submit" class="btn" style="background-color: blue; color: white; width: 50%;"> 
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn" style="background-color: blue; color: white; width: 50%;">
                                     {{ __('Login') }}
                                 </button>
                             </div>
@@ -72,5 +163,6 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</div> --}}
+{{-- @endsection --}}
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,125 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>Register</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
+
+    <!-- Bootstrap CSS via CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <!-- Feather Icons via CDN -->
+    <link rel="stylesheet" href="https://unpkg.com/feather-icons/dist/feather.css">
+
+    <!-- Font Awesome CSS via CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+    <link rel="stylesheet" href="{{ asset('css/register-style.css') }}">
+</head>
+
+<body>
+
+<div class="main-wrapper login-body">
+        <div class="login-wrapper">
+            <div class="container">
+                <p class="account-subtitle">Enter account details</p>
+
+                <div class="loginbox row">
+                    <div class="login-left col-lg-6">
+                        <form action="{{ route('register') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label>Full Name <span class="login-danger">*</span></label>
+                                <input class="form-control" type="text" name="name" value="{{ old('name') }}"
+                                    required autocomplete="name" autofocus>
+                                @error('name')
+                                <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label>Email <span class="login-danger">*</span></label>
+                                <input class="form-control" type="email" name="email" value="{{ old('email') }}"
+                                    required autocomplete="email">
+                                @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label>Password <span class="login-danger">*</span></label>
+                                <input class="form-control pass-input" type="password" name="password" required
+                                    autocomplete="new-password">
+                                @error('password')
+                                <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label>Confirm password <span class="login-danger">*</span></label>
+                                <input class="form-control pass-confirm" type="password"
+                                    name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                    </div>
+
+                    <div class="login-right col-lg-6">
+                        <div class="login-right-wrap">
+                            <div class="form-group">
+                                <label for="role">Role</label>
+                                <select id="role" name="role" class="form-control" required>
+                                    <option value="siswa">Siswa</option>
+                                    <option value="guru">Guru</option>
+                                    <option value="admin">Admin</option>
+                                </select>
+                            </div>
+
+                            <div id="siswaInput" class="form-group">
+                                <label for="SiswaID">Siswa ID</label>
+                                <input type="text" id="SiswaID" name="SiswaID" class="form-control">
+                            </div>
+
+                            <div id="guruInput" class="form-group">
+                                <label for="GuruID">Guru ID</label>
+                                <input type="text" id="GuruID" name="GuruID" class="form-control">
+                            </div>
+
+                            <div id="adminInput" class="form-group">
+                                <label for="AdminID">Admin ID</label>
+                                <input type="text" id="AdminID" name="AdminID" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="dont-have">Already Registered? <a href="{{ route('login') }}">Login</a></div>
+                <div class="form-group mb-0">
+                    <button class="btn btn-primary btn-block" type="submit">Register</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery via CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap JS via CDN -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Feather Icons JS via CDN -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+
+</html> --}}
+
+
 @extends('layouts.app')
 
 @section('content')
@@ -62,8 +184,8 @@
                         </div>
 
                         <div class="mb-0 row d-flex justify-content-center">
-                            <div class="col-md-6 text-center"> 
-                                <button type="submit" class="btn btn-block" style="background-color: blue; color: white; width: 50%;"> 
+                            <div class="col-md-6 text-center">
+                                <button type="submit" class="btn btn-block" style="background-color: blue; color: white; width: 50%;">
                                     {{ __('Register') }}
                                 </button>
                             </div>
